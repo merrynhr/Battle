a0f7ca68f098836ee6cf6904b8c5f28f505fb69e
Merryn <merrynhr@gmail.com>
2021-06-09 15:18:01 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+# in app.rb
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base 
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+    get '/' do
+        'Hello Battle!'
+    end
+
+    # start the server  if ruby file executed directly
+    run! if app_file == $0
+end
