526aca8d8e048b9994721b5d8f811999a4a35669
Merryn <merrynhr@gmail.com>
2021-06-09 17:06:43 +0100

    Post/redirect/Get pattern

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base 
+    enable :sessions
+
     configure :development do
         register Sinatra::Reloader
     end
@@ -13,8 +15,17 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-        @player_1_name = params[:player_1_name]
-        @player_2_name = params[:player_2_name]
+        # @player_1_name = params[:player_1_name]
+        # @player_2_name = params[:player_2_name]
+        session[:player_1_name] = params[:player_1_name]
+        session[:player_2_name] = params[:player_2_name]
+        # erb :play
+        redirect '/play'
+    end
+
+    get '/play' do
+        @player_1_name = session[:player_1_name]
+        @player_2_name = session[:player_2_name]
         erb :play
     end
 
